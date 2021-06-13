af8e291a6bbe74e3903dec40bf1fbb7cdffce5a3
karthik santhosh <developer.karthiksanthosh@gmail.com>
2021-06-13 23:17:47 +0530

    Added post model

--- post/models.py
+++ post/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from account.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+  author = models.ForeignKey(User, on_delete=models.CASCADE)
+  title = models.CharField(max_length=255)
+  image = models.CharField(upload_to='uploads/post/images/%Y/%m/%d/')
+  body = models.TextField(max_length=4000)
+  created = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+    return str(self.title + 'By, ' +self.author)
+
+
+
--- src/settings.py
+++ src/settings.py
@@ -131,3 +131,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+AUTH_USER_MODEL = 'account.User'
