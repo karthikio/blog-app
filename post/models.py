b09dd2aca0f3340dbb084d67d30b9c86583a0266
karthik santhosh <developer.karthiksanthosh@gmail.com>
2021-06-14 20:52:09 +0530

    Created profiles model

--- post/models.py
+++ post/models.py
@@ -5,7 +5,7 @@ from account.models import User
 class Post(models.Model):
   author = models.ForeignKey(User, on_delete=models.CASCADE)
   title = models.CharField(max_length=255)
-  image = models.CharField(upload_to='uploads/post/images/%Y/%m/%d/')
+  image = models.ImageField(upload_to='uploads/post/images/%Y/%m/%d/')
   body = models.TextField(max_length=4000)
   created = models.DateTimeField(auto_now=True)
 
---
+++ user_profile/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.db.models.base import Model
+from account.models import User
+
+def upload_to(instance, filename):
+    return 'images/%s/%s' % (instance.user.user.username, filename)
+
+class Profile(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  profile_pic = models.ImageField(upload_to=upload_to)
+  name = models.CharField(max_length=255)
+  bio = models.TextField(max_length=500)
+  privacy = models.BooleanField(default=False)
+
