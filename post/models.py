becbf24b1261cc257168d4921a85df12a68a091c
karthik santhosh <developer.karthiksanthosh@gmail.com>
2021-06-15 12:53:38 +0530

    Fixed bug, created filesystem

--- post/models.py
+++ post/models.py
@@ -1,19 +1,19 @@
 from django.db import models
 from account.models import User
 
-def post_upload_path(instance, filename):
-    return 'uploads/posts/images/%Y/%m/%d/' % (instance.user.user.username, filename)
 
+def upload_path(instance, filename):
+  return 'images/{0}/posts/{1}'.format(instance.author.username, filename)
 
 class Post(models.Model):
   author = models.ForeignKey(User, on_delete=models.CASCADE)
   title = models.CharField(max_length=255)
-  image = models.ImageField(upload_to=post_upload_path)
+  image = models.ImageField(upload_to=upload_path, blank=True, null=True)
   body = models.TextField(max_length=4000)
   created = models.DateTimeField(auto_now=True)
 
   def __str__(self):
-    return str(self.title + 'By, ' +self.author)
+    return "{title}- {author}".format(title=self.title, author=self.author)
 
 
 
--- user_profile/models.py
+++ user_profile/models.py
@@ -2,13 +2,17 @@ from django.db import models
 from django.db.models.base import Model
 from account.models import User
 
-def profile_upload_path(instance, filename):
-    return 'uploads/profile_pic/images/%s/%s' % (instance.user.user.username, filename)
+
+def upload_path(instance, filename):
+    return  'images/{0}/profile/{1}'.format(instance.user.username, filename)
+
 
 class Profile(models.Model):
   user = models.OneToOneField(User, on_delete=models.CASCADE)
-  profile_pic = models.ImageField(upload_to=profile_upload_path)
+  profile_pic = models.ImageField(upload_to=upload_path)
   name = models.CharField(max_length=255)
   bio = models.TextField(max_length=500)
   privacy = models.BooleanField(default=False)
 
+  def __str__(self):
+    return self.user.username
