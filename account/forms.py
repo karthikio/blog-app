00d20a1d0ed21ef0725dd6113bf0e5569900e69e
karthik santhosh <developer.karthiksanthosh@gmail.com>
2021-06-18 18:09:14 +0530

    Added user register form

--- account/forms.py
+++ account/forms.py
@@ -5,38 +5,93 @@ from .models import User
 
 
 class UserAdminCreationForm(forms.ModelForm):
-    password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
-    password2 = forms.CharField(label="Confirm Password", widget=forms.PasswordInput)
+  password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
+  password2 = forms.CharField(label="Confirm Password", widget=forms.PasswordInput)
 
-    class Meta:
-        model = User
-        fields = ('username', 'email',)
+  class Meta:
+    model = User
+    fields = ('username', 'email',)
 
-    def clean_password(self):
-        password1 = self.cleaned_data.get('password1')
-        password2 = self.cleaned_data.get('password2')
+  def clean_password(self):
+    password1 = self.cleaned_data.get('password1')
+    password2 = self.cleaned_data.get('password2')
+    if password1 and password2 and password1 != password2:
+      raise forms.ValidationError('Passwords didn\'t match!')
+    return password2
 
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError('Passwords didn\'t match!')
-        return password2
-
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.set_password(self.cleaned_data['password2'])
-        if commit:
-            user.save()
-        return user
+  def save(self, commit=True):
+    user = super().save(commit=False)
+    user.set_password(self.cleaned_data['password2'])
+    if commit:
+      user.save()
+    return user
 
 
 class UserAdminChangeForm(forms.ModelForm):
-    password = ReadOnlyPasswordHashField()
+  password = ReadOnlyPasswordHashField()
 
-    class Meta:
-        model = User
-        fields = ('username', 'email', 'password', 'is_active', 'staff', 'admin')
+  class Meta:
+    model = User
+    fields = ('username', 'email', 'password', 'is_active', 'staff', 'admin')
 
-    def clean_password(self):
-        return self.initial['password']
+  def clean_password(self):
+    return self.initial['password']
+
+
+
+# register form
+class RegisterForm(forms.ModelForm):
+  username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-input','placeholder': 'Username'}))
+  email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-input','placeholder': 'Email Id'}))
+  password1 = forms.CharField(widget=forms.PasswordInput(
+    attrs = {
+      'class': 'form-input',
+      'placeholder': 'Password'
+    }
+  )) 
+
+  password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput(
+    attrs = {
+      'class': 'form-input',
+      'placeholder': 'Confirm Password'
+    }
+  ))
+
+  class Meta:
+    model = User
+    fields = ('email', 'username',)
+
+  def clean_email(self):
+    email = self.cleaned_data.get('email')
+    qs = User.objects.filter(email=email)
+    if qs.exists():
+      raise forms.ValidationError('This email is taken.')
+    return email
+
+  def clean_username(self):
+    username = self.cleaned_data.get('username')
+    qs = User.objects.filter(username=username)
+    if qs.exists():
+      raise forms.ValidationError('This username is taken.')
+    return username
+    
+  def clean_password2(self):
+    # checking password1 == password2
+    password1 = self.cleaned_data.get('password1')
+    password2 = self.cleaned_data.get('password2')
+    if password1 and password2 and password1 != password2:
+      raise forms.ValidationError("Passwords didn't match")
+    return password2
+
+  def save(self, commit=True):
+    # Save the provided password in hashed format
+    user = super(RegisterForm, self).save(commit=False)
+    user.set_password(self.cleaned_data["password2"])
+    if commit:
+      user.save()
+    return user
+
+
 
 
 # login form
