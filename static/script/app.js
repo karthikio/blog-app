26b70e956b975da4a6175ae5ad697648d12ad213
karthik santhosh <developer.karthiksanthosh@gmail.com>
2021-06-24 23:06:12 +0530

    Added copy to clipboard

--- static/script/app.js
+++ static/script/app.js
@@ -19,3 +19,12 @@ fetch('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=
   .then(res => res.json())
   .then(data => console.log(data))
   .catch(err => console.log(err))
+
+
+function myFunction() {
+  var copyText = document.getElementById("myInput");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  alert("Copied the text: " + copyText.value);
+}
