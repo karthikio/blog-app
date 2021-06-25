1d3965d99a126d08386d4696e5c1b77b3e78624d
karthik santhosh <developer.karthiksanthosh@gmail.com>
2021-06-25 20:17:08 +0530

    Added custom password reset email template

--- static/script/app.js
+++ static/script/app.js
@@ -10,21 +10,6 @@ window.addEventListener('load', () => {
     loader.style.display = 'none';
     blocks.classList.remove('hidden');
   }, 2000)
-
-  
 })
 
 
-fetch('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=API_KEY')
-  .then(res => res.json())
-  .then(data => console.log(data))
-  .catch(err => console.log(err))
-
-
-function myFunction() {
-  var copyText = document.getElementById("myInput");
-  copyText.select();
-  copyText.setSelectionRange(0, 99999)
-  document.execCommand("copy");
-  alert("Copied the text: " + copyText.value);
-}
