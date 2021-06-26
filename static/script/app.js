22e9e6c231e4cfec5ea813ec577d215586675606
karthik santhosh <developer.karthiksanthosh@gmail.com>
2021-06-26 15:22:25 +0530

    Added hamburger menu

--- static/script/app.js
+++ static/script/app.js
@@ -1,15 +1,23 @@
 let loaderContainer = document.getElementById('loader-container');
 let loader = document.getElementById('loader');
 let blocks = document.querySelector('#blocks');
+let menu = document.getElementById('id_menu_container');
+let menu2 = document.getElementById('id_menu_container_2');
+let navSmall = document.getElementById('nav-small');
 
 
 window.addEventListener('load', () => {
   // loader
   setTimeout(() => {
     loaderContainer.style.display = 'none';
-    loader.style.display = 'none';
     blocks.classList.remove('hidden');
   }, 2000)
 })
 
+menu.addEventListener('click', () =>{
+  navSmall.style.width = '60%';
+})
 
+menu2.addEventListener('click', () => {
+  navSmall.style.width = '0';
+})
